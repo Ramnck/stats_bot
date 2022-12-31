1916c4facc2715496cf51646ccd58f3c03eb6612
Ramnck <r.karet@gmail.com>
2022-12-31 17:01:36 +0300

    deploy hotfix

--- app/src/base.py
+++ app/src/base.py
@@ -1,9 +1,10 @@
 from aiogram.types import Message
-
+from aiogram import Bot
 from .settings import get_settings
 
 settings = get_settings()
 
+bot = Bot(settings.TOKEN)
 
 def is_based(msg: Message) -> bool:
     return msg.from_user.id in settings.CHATS
